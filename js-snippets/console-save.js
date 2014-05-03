94c9a29f4a92c5b80135561655c4de1ff613bb3d
rballen <robert.burch.allen@gmail.com>
2014-05-03 13:01:08 -0400

    chrome dev tools snippets

---
+++ js-snippets/console-save.js
@@ -0,0 +1,35 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+r),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+i), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+l)
+ */
+
+(function(console){
+
+    console.save = function(data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'console.json'
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console)
